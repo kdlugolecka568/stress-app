cf73831307d8e9a3adb3e4acb61e74a4b8a520c2
Katarzyna Długołęcka <kasia@MacBook-Air-Katarzyna.local>
2026-02-05 17:45:37 +0100

    Refactor: move risk_level to utils module

---
+++ utils.py
@@ -0,0 +1,7 @@
+from utils import risk_level
+
+def test_risk_level_ranges():
+    assert risk_level(0.1) == "niskie"
+    assert risk_level(0.3) == "umiarkowane"
+    assert risk_level(0.5) == "podwyższone"
+    assert risk_level(0.8) == "wysokie"
